cc43f4b944ceda3a40a6542ca666555b455e0eac
AlexMP-Dev <songoku1093@gmail.com>
2023-12-02 16:17:37 -0500

    loginpage v2

--- src/app/loginpage/loginpage.component.ts
+++ src/app/loginpage/loginpage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-loginpage',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './loginpage.component.html',
   styleUrl: './loginpage.component.css'
 })
