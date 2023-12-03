af885acc5f04669f25ca77a2f9689fe5c5e6b749
Mauricio Fow <mauriciofow@gmail.com>
2023-12-03 19:46:38 +0200

    basic folder structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { LoginpageComponent } from './loginpage/loginpage.component';
+import { LoginpageComponent } from './pages/login/loginpage.component';
 
 export const routes: Routes = [
   {path:'login', component: LoginpageComponent},
