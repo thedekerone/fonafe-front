6bf08f0280749ee6c9fb300fdc5817c203d72eb3
Mauricio Fow <mauriciofow@gmail.com>
2023-12-05 17:24:38 +0200

    created blog page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { LoginpageComponent } from './pages/login/loginpage.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CreateMaintenancePageComponent } from './create-maintenance-page/create-maintenance-page.component';
-import { VideoComponent } from './ages/video/video.component';
+import { VideoComponent } from './pages/video/video.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
 export const routes: Routes = [
   {path: '', redirectTo:'home', pathMatch: 'full'},
   {path:'login', component: LoginpageComponent},
   {path:'home', component: HomeComponent},
   {path:'video', component: VideoComponent},
+  {path:'sala-de-prensa', component: BlogComponent},
   {path:'**', component: CreateMaintenancePageComponent},
 ];
--- src/app/components/blog/header-line/header-line.component.ts
+++ src/app/components/blog/header-line/header-line.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header-line',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './header-line.component.css'
 })
 export class HeaderLineComponent {
-
+  @Input() title = ""
 }
