11498657c2b24d041329e473b0468175c6649b30
AlexMP-Dev <songoku1093@gmail.com>
2023-12-04 23:09:10 -0500

    videopage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginpageComponent } from './pages/login/loginpage.component';
 import { HomeComponent } from './pages/home/home.component';
+import { VideoComponent } from './pages/video/video.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path:'login', component: LoginpageComponent},
   {path:'home', component: HomeComponent},
+  {path:'video', component: VideoComponent},
 
 ];
