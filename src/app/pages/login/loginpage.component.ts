d676d6e26dd0423700bdf4942ec012dbdb5d34df
Mauricio Fow <mauriciofow@gmail.com>
2023-12-12 19:21:46 +0200

    last version

--- src/app/pages/login/loginpage.component.ts
+++ src/app/pages/login/loginpage.component.ts
@@ -2,24 +2,26 @@ import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { FormsModule } from '@angular/forms';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-loginpage',
   standalone: true,
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule, ButtonComponent],
   templateUrl: './loginpage.component.html',
   styleUrl: './loginpage.component.css'
 })
 export class LoginpageComponent {
-
+  loading = false
 
   constructor(private authService: AuthService, private router: Router) { }
   login(form: any) {
-
     const { email, password } = form;
     if (email && password) {
+      this.loading = true
       this.authService.signIn(email, password)
         .then(res => {
+          this.loading = false
           // Handle the response
           this.router.navigate(["/home"])
         })
